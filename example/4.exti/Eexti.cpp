3d1e34026b3f89e8d3b43bf0c163475d13a544fb
eboxmaker <995207301@qq.com>
2016-01-22 22:47:24 +0800

    添加外部中断例程说明

--- example/4.exti/Eexti.cpp
+++ example/4.exti/Eexti.cpp
@@ -13,9 +13,9 @@ Copyright 2015 shentq. All Rights Reserved.
 
 uint32_t xx;
 
-EXTIx ex(&PA8,EXTI_Trigger_Falling);
+EXTIx ex(&PA8,EXTI_Trigger_Falling);//创建一个检测下降沿的外部中断对象
 
-void exit()
+void exit()//中断调用用户事件
 {
 	xx++;
 	uart1.printf("\r\nxx = %d",xx);
@@ -24,9 +24,9 @@ void setup()
 {
 	ebox_init();
 	uart1.begin(9600);
-	ex.begin();
-	ex.attachInterrupt(exit);
-	ex.interrupt(ENABLE);
+	ex.begin();//初始化外部中断配置
+	ex.attachInterrupt(exit);//绑定中断用户事件
+	ex.interrupt(ENABLE);//开启中断
 }
 
 
