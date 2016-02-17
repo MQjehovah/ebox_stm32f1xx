55f81a585005781aa5617a12d3dd39c6c4daa8b7
eboxmaker <995207301@qq.com>
2016-02-17 21:29:45 +0800

    更新ebox版本1.1

--- user/main.cpp
+++ user/main.cpp
@@ -6,57 +6,92 @@ date   : 2015/7/5
 
 Copyright 2015 shentq. All Rights Reserved.
 */
+#include "freertos.h"
+#include "task.h"
+#include "queue.h"
 
 //STM32 RUN IN eBox
 #include "ebox.h"
+#define LED_0 0
+#define LED_1 1
+#define LED_2 2
+#define LED_3 3
 
-#define N 10
-u8 src_array[N];
-u8 array_len = N - 1;
-
-void random_init()
+void led_on(int n) 
 {
-    for(int i = 0; i < N; i++)
-        src_array[i] = i;
+	switch (n) 
+	{
+		case LED_0:
+			PB8 = 1;
+			break;
+		case LED_1:
+			PB9 = 1;
+			break;
+		case LED_2:
+			PB10 = 1;
+			break;
+		default:
+			break;
+	}
 }
-u8 random_no_repeat()
-{
-    u8 temp;
 
-    if(array_len == 0)
-        return src_array[0];
-    else
+void led_off(int n)
+{
+	switch (n) 
+	{
+		case LED_0:
+			PB8 = 0;
+			break;
+		case LED_1:
+			PB9 = 0;
+			break;
+		case LED_2:
+			PB10 = 0;
+			break;
+		default:
+			break;
+	}
+}
+
+
+static void vLEDTask( void *pvParameters )
+{
+    while(1)
     {
-        u8 seed = random(array_len);
-        temp = src_array[seed];
-        if(seed != (array_len))
-            src_array[seed] = src_array[array_len];
-        array_len--;
-        return temp;
+        led_on((int)pvParameters);
+        vTaskDelay(500/portTICK_RATE_MS);
+        led_off((int)pvParameters);
+        vTaskDelay(500/portTICK_RATE_MS);
     }
 }
-
 
 void setup()
 {
-    ebox_init();
-    uart1.begin(9600);
-    random_init();
-    for(int i = 0; i < N; i++)
-    {
-        uart1.printf("%d:random=%d\r\n",i,random_no_repeat());
-        delay_ms(100);
-    }
-	
+	ebox_init();
+	PB8.mode(OUTPUT_PP);
+	PB9.mode(OUTPUT_PP);
+	PB10.mode(OUTPUT_PP);
+    
+    set_systick_user_event_per_sec(configTICK_RATE_HZ);
+    attch_systick_user_event(xPortSysTickHandler);
+    
+    xTaskCreate( vLEDTask,  "LED0", configMINIMAL_STACK_SIZE, (void *)0, tskIDLE_PRIORITY+3, NULL );
+    xTaskCreate( vLEDTask, "LED1", configMINIMAL_STACK_SIZE, (void *)1, tskIDLE_PRIORITY+3, NULL );
+    xTaskCreate( vLEDTask, "LED2", configMINIMAL_STACK_SIZE, (void *)2, tskIDLE_PRIORITY+3, NULL );
+
+    /* Start the scheduler. */
+    vTaskStartScheduler();
+
 }
 int main(void)
 {
-    setup();
-    while(1)
-    {
-    }
+	setup();
+	while(1)
+	{
+	}
+
+    return 0;
+
 }
-
-
 
 
